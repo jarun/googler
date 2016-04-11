89dde93b3cc300272d5b6f466b0aca64f3e966cd
Arun Prakash Jana <engineerarun@gmail.com>
2016-04-11 23:09:34 +0530

    Indicate option arguments.

--- auto-completion/fish/googler.fish
+++ auto-completion/fish/googler.fish
@@ -5,13 +5,13 @@
 #   Arun Prakash Jana <engineerarun@gmail.com>
 #
 complete -c googler -s s -l start  -r      --description 'start at the Nth result'
-complete -c googler -s n -l count  -r      --description 'show N results (default 10)'
+complete -c googler -s n -l count  -r      --description 'show specified number of results (default 10)'
 complete -c googler -s N -l news           --description 'show results from news section'
-complete -c googler -s c -l tld    -r      --description 'country-specific search with top-level domain .TLD'
-complete -c googler -s l -l lang   -r      --description 'display in language LANG'
+complete -c googler -s c -l tld    -r      --description 'country-specific search with top-level domain'
+complete -c googler -s l -l lang   -r      --description 'display in specified language'
 complete -c googler -s x -l exact          --description 'disable automatic spelling correction'
 complete -c googler -s C -l nocolor        --description 'disable color output'
 complete -c googler -s j -l first -l lucky --description 'open the first result in a web browser'
-complete -c googler -s t -l time   -r      --description 'time limit search [e.g. h5, d5, w5, m5, y5]'
+complete -c googler -s t -l time   -r      --description 'time limit search (h/d/w/m/y + number)'
 complete -c googler -s w -l site   -r      --description 'search a site using Google'
 complete -c googler -s d -l debug          --description 'enable debugging'
